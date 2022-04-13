91ef60c9ea1e51335f84a86bd6431bf73c363ba1
aaronvelezcoronado <aaronvelezcoronado@icloud.com>
2022-04-13 11:37:17 -0700

    recoded game version

--- rps.js
+++ rps.js
@@ -1,54 +1,77 @@
-//function that will randomly return rock, paper or scissors/
+//STEP 1: GET USERINPUT
 
+const getUserChoice = userInput => {
+    const promptAnswer = prompt('Choose your weapon!')
+    userInput = promptAnswer.toLowerCase()
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors')
+    {
+        return userInput
+    } else  {
+        return 'an invalid weapon'
+    }
+}
+//console.log(getUserChoice())
 
-function computerSelection() {
-    let randomNumber = Math.floor(Math.random() * 3)
+//GET COMPUTER INPUT USING A RANDOM NUM FROM 0-2 AND ASSIGNING VALUE TO EACH RESULT
 
-    if (randomNumber === 0) {
-        return 'rock'
-    } else if (randomNumber === 1) {    
-        return 'paper'
+const getComputerChoice = botChoice => {
+    computerChoice = Math.floor(Math.random() * 3)
+    if (computerChoice === 0) {
+        return 'paper' } 
+    if (computerChoice === 1) {
+        return 'rock' 
     } else {
         return 'scissors'
     }
+   } 
+
+
+// DETERMINE WINNER USING TWO VARIABLES THAT WILL BE ASSINED TO CORRESPONDING FUNCTIONS
+   const determineWinner = (userChoice, computerChoice) => {
+       if (userChoice === computerChoice) {
+           return 'ITS A TIE!';
+       } 
+        else if (userChoice === 'rock' && computerChoice === 'paper') {
+           return 'Sorry, computer won!'
+       }
+       else if (userChoice === 'paper' && computerChoice === 'scissors') {
+           return 'Sorry computer won!'
+       }
+       else if (userChoice === 'scissors' && computerChoice === 'rock') {
+           return 'Sorry, computer won!'
+       } 
+       else if (userChoice === 'rock' && computerChoice === 'scissors') {
+           return 'You won!'
+       }
+       else if (userChoice === 'paper' && computerChoice === 'rock') {
+           return 'You won!'
+       }
+       else if (userChoice === 'scissors' && computerChoice === 'paper') {
+           return 'You won!'
+       } else { 
+           return 'Try following the rules next time!'
+
+       }
+       
+   }
+   //console.log(determineWinner('paper', 'rock'))
+
+   function playGame() {
+    const userChoice = getUserChoice()
+    const computerChoice = getComputerChoice()
+    console.log(`You threw ${userChoice} and the computer threw ${computerChoice}`)
+
+    console.log(determineWinner(userChoice, computerChoice))
 
 }
 
-//console.log(computerSelection()) returns number 0-2
-// computerSelection function returns rock, paper or scissors depending on result of the randumNumber
-//console.log(computerSelection())
+    //console.log(playGame())
 
+    
+    const fiveRoundGame = () => {
+    for (let i = 0; i < 5; i++) {
+        playGame()
+     }
 
-/*function playerSelection() {
-    let playerChoice = prompt("Choose your weapon!")
-
-    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-        let playerAnswer = playerChoice.toLowerCase;
-    } else {
-        return 'not a weapon!'
     }
-} */
-
-//console.log(playerSelection())
-//let computerAnswer = computerSelection()
-//let playerWeapon = playerSelection()
-//console.log(computerAnswer)
-//console.log(playerWeapon)
-
-function playGame(playerWeapon, computerAnswer) {
-    if (computerAnswer === playerWeapon) {
-        return 'ITS A TIE!'
-    } else if (playerWeapon === 'rock' && computerAnswer === 'paper') {
-        return `You chose ${playerWeapon} and computer chose ${computerAnswer}! You lose!`
-    } else if (playerWeapon === 'scissors' && computerAnswer === 'rock') {
-        return `You chose ${playerWeapon} and computer chose ${computerAnswer}! You lose!`
-    } else if (playerWeapon === 'paper' && computerAnswer === 'scissors') {
-        return `You chose ${playerWeapon} and computer chose ${computerAnswer}! You lose!`
-    } else {
-        return `You chose ${playerWeapon} and computer chose ${computerAnswer}. You Win!` }
-
-}
-const playerWeapon = 'ROCK';
-const computerAnswer = computerSelection()
-console.log(playGame(playerWeapon, computerAnswer))
-
+    console.log(fiveRoundGame())
